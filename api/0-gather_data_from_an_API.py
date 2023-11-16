e204303f8ef783567f42f46284c99be39e817c41
matternstsr <matternstsr@gmail.com>
2023-11-16 10:42:44 -0600

    oops reverted when employee was employer lol going to the right one.

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -1,15 +1,15 @@
 #!/usr/bin/python3
 """returns info todo list progress based on the userid."""
-from sys import argv
 import json
 import urllib.request
+from sys import argv
 
 if __name__ == '__main__':
-    api_url = 'https://jsonplaceholder.typicode.com'
-    user_id = argv[1]
+    base_api_url = 'https://jsonplaceholder.typicode.com'
+    target_user_id = argv[1]
 
     with urllib.request.urlopen(
-        f'{api_url}/users/{user_id}/todos?_expand=user'
+        f'{base_api_url}/users/{target_user_id}/todos?_expand=user'
     ) as response:
         if response.getcode() == 200:
             todo_data = json.loads(response.read())
@@ -19,7 +19,7 @@ if __name__ == '__main__':
             num_completed_tasks = len(completed_tasks)
             total_tasks = len(todo_data)
 
-            first_str = f"Employer {employee_name} is done with tasks"
+            first_str = f"Employee {employee_name} is done with tasks"
 
             print(f"{first_str} ({num_completed_tasks}/{total_tasks}):")
             for task in completed_tasks:
