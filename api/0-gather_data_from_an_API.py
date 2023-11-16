7c8ccb54befca54074003675a3ad8729bac0a6ef
matternstsr <matternstsr@gmail.com>
2023-11-16 10:06:06 -0600

    fixed the pep8 issues

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -8,19 +8,18 @@ apiforurl = 'https://jsonplaceholder.typicode.com'
 if __name__ == '__main__':
     user_info_response = requests.get(f"{apiforurl}/users/{argv[1]}")
     user_data = user_info_response.json()
-    
+
     tasks_response = requests.get(f"{apiforurl}/todos?userId={argv[1]}")
-    task_info = tasks_response.json()
-    
+    task_info  tasks_response.json()
+
     completed_tasks = [task for task in task_info if task['completed']]
-    
+
     emp_name = user_data["name"]
     num_completed_tasks = len(completed_tasks)
     all_tasks = len(task_info)
-    
+
     print("Employee {} is done with tasks({}/{}):".format(
         emp_name, num_completed_tasks, all_tasks))
-    
+
     for task in completed_tasks:
         print(f"\t {task['title']}")
-
