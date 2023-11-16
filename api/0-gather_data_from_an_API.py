59304f0538b91809904c3baef15cb208111317e8
matternstsr <matternstsr@gmail.com>
2023-11-16 08:28:50 -0600

    started 0 with lots of errors to the url

---
+++ api/0-gather_data_from_an_API.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+'''for a given emp ID, returns info about his/her TODO list progress.'''
+import json
+import urllib.request
+from sys import argv
+
+if __name__ == '__main__':
+    url = 'https://jsonplaceholder.typicode.com'
+    user_id = argv[1]
+
+    with urllib.request.urlopen(f'{url}/{user_id}/todos?_expand=user') as response:
+        if response.getcode() == 200:
+            jsondata = json.loads(response.read())
+            name = jsondata[0]['user']['name']
+            donetasks = [task for task in jsondata if task['completed']]
+            numberdone = len(donetasks)
+            alltasks = len(jsondata)
+
+            first_str = f"Employer {name} is done with tasks"
+
+            print(f"{first_str} ({numberdone}/{alltasks}):")
+            for task in donetasks:
+                print(f"\t {task['title']}")
+        else:
+            print(f"Error: {response.getcode()}")
