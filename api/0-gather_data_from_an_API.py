47c49ee0bfaf23a17ce57417a3bf05d07e400f17
matternstsr <matternstsr@gmail.com>
2023-11-16 09:18:49 -0600

    changed the comment and imports alpha

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
-'''for a given emp ID, returns info about his/her TODO list progress.'''
+"""for a given emp ID, returns info about his/her TODO list progress."""
+
+from sys import argv
 import json
 import urllib.request
-from sys import argv
+
 
 if __name__ == '__main__':
     url = 'https://jsonplaceholder.typicode.com'
