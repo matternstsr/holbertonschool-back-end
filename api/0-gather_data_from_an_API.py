896ebe974bdf17988d718b4ad2a1cce3951edfee
matternstsr <matternstsr@gmail.com>
2023-11-16 10:29:09 -0600

    broke line 11 to smaller limit

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -7,11 +7,14 @@ from sys import argv
 if __name__ == '__main__':
     api_url = 'https://jsonplaceholder.typicode.com'
     user_id = argv[1]
-
-    with urllib.request.urlopen(f'{api_url}/users/{user_id}/todos?_expand=user') as response:
+    
+    with urllib.request.urlopen(
+    f'{api_url}/users/{user_id}/todos?_expand=user'
+    ) as response:
         if response.getcode() == 200:
             todo_data = json.loads(response.read())
             employee_name = todo_data[0]['user']['name']
+
             completed_tasks = [task for task in todo_data if task['completed']]
             num_completed_tasks = len(completed_tasks)
             total_tasks = len(todo_data)
